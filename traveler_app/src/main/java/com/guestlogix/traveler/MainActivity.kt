3f60b795f77056449e52e484b37dfb16f69137fb
Sayyam Mehmood <smehmood@itadmins-MacBook-Pro-3.local>
2018-11-27 22:53:10 -0500

    moves initialization to application

--- traveler_app/src/main/java/com/guestlogix/traveler/MainActivity.kt
+++ traveler_app/src/main/java/com/guestlogix/traveler/MainActivity.kt
@@ -1,16 +1,12 @@
 package com.guestlogix.traveler
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.guestlogix.travelercorekit.sdk.Traveler
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        Traveler.initialize("apiKey", applicationContext)
-
     }
 }
