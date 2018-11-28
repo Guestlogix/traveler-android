c186acca9bcb3702d62767a5a7c82dfd0332b23a
Sayyam Mehmood <smehmood@itadmins-MacBook-Pro-3.local>
2018-11-27 22:53:10 -0500

    moves initialization to application

---
+++ traveler_app/src/main/java/com/guestlogix/traveler/application/TravelerApplication.java
@@ -0,0 +1,14 @@
+package com.guestlogix.traveler.application;
+
+import android.app.Application;
+import com.guestlogix.travelercorekit.sdk.Traveler;
+
+public class TravelerApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Traveler.initialize("apiKey", getApplicationContext());
+    }
+
+}
