49fc51fa803a42772f14e4e33b822d87398b4190
Sayyam Mehmood <smehmood@itadmins-MacBook-Pro-3.local>
2018-11-30 17:06:19 -0500

    stitched together task manager and router

---
+++ traveler_core_kit/src/main/java/com/guestlogix/task/JsonObjectMapper.java
@@ -0,0 +1,53 @@
+package com.guestlogix.task;
+
+import com.guestlogix.network.MappingFactory;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class JsonObjectMapper<T> implements NetworkTask.ResponseHandler {
+    private MappingFactory<T> mMappingFactory;
+    private Callback<T> mCallback;
+
+    public interface Callback<T> {
+        void onSuccess(T model);
+        void onError(Error error);
+    }
+
+    public JsonObjectMapper(MappingFactory<T> mappingFactory, Callback<T> callback) {
+        mMappingFactory = mappingFactory;
+        mCallback = callback;
+    }
+
+    @Override
+    public void onHandleResponse(InputStream stream) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        StringBuilder builder = new StringBuilder();
+
+        // TODO: Rewrite this using JSONReader
+
+        // TODO: handle error correctly
+
+        try {
+            String inputStr;
+            while ((inputStr = reader.readLine()) != null)
+                builder.append(inputStr);
+
+            JSONObject jsonObject = new JSONObject(builder.toString());
+
+            T model = mMappingFactory.instantiate(jsonObject);
+
+            mCallback.onSuccess(model);
+        } catch (IOException e) {
+            mCallback.onError(new Error());
+        } catch (JSONException e) {
+            mCallback.onError(new Error());
+        } catch (com.guestlogix.network.MappingException e) {
+            e.printStackTrace();
+        }
+    }
+}
