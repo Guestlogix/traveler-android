db99a7b388f44e2ba5994ceffcf0192e622262af
Sayyam Mehmood <sayyam.mehmood@gmail.com>
2018-12-07 16:29:54 -0500

    adds authentication feature

--- traveler_core_kit/src/main/java/com/guestlogix/task/Router.java
+++ traveler_core_kit/src/main/java/com/guestlogix/task/Router.java
@@ -4,8 +4,10 @@ import android.content.Context;
 import android.util.Log;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,19 +15,23 @@ import java.util.Map;
 public class Router {
     static final String BASE_URL = "https://guest-api-dev-1.guestlogix.io";
 
-    private static URL createURL(String path) {
+    private static URL createURL(String path, Map<?, ?> queryParams) {
         try {
-            return new URL(BASE_URL + path);
+            return new URL(BASE_URL + path + "?" + urlEncodeUTF8(queryParams));
         } catch (MalformedURLException e) {
             Log.e("Router", "Bad URL: " + BASE_URL + path);
             return null;
         }
     }
 
+    private static URL createURL(String path) {
+        return createURL(path, null);
+    }
+
     public static NetworkTask.Request authenticate(String apiKey, Context context) {
 
         Map<String, String> payload = new HashMap<>();
-        payload.put("deviceId", "android_123");
+        payload.put("deviceId", "android_678");
         payload.put("osVersion", "oreo");
         payload.put("language", "en");
         payload.put("locale", "en_POSIX");
@@ -35,5 +41,40 @@ public class Router {
         UnauthenticatedRequest request = new UnauthenticatedRequest(NetworkTask.Request.Method.POST, createURL("/auth/token"), apiKey, new JSONObject(payload));
 
         return request;
+    }
+
+    public static NetworkTask.Request getCatalogue(String token, String id) {
+
+        Map<String, String> queryParams = new HashMap<>();
+        queryParams.put("flight-ids", id);
+
+
+        AuthenticatedRequest request = new AuthenticatedRequest(NetworkTask.Request.Method.GET, createURL("/catalog", queryParams), token);
+
+        return request;
+    }
+
+    private static String urlEncodeUTF8(Map<?, ?> map) {
+        if (null == map)
+            return "";
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<?, ?> entry : map.entrySet()) {
+            if (sb.length() > 0) {
+                sb.append("&");
+            }
+            sb.append(String.format("%s=%s",
+                    urlEncodeUTF8(entry.getKey().toString()),
+                    urlEncodeUTF8(entry.getValue().toString())
+            ));
+        }
+        return sb.toString();
+    }
+
+    private static String urlEncodeUTF8(String s) {
+        try {
+            return URLEncoder.encode(s, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new UnsupportedOperationException(e);
+        }
     }
 }
--- traveler_core_kit/src/main/java/com/guestlogix/travelercorekit/sdk/Traveler.java
+++ traveler_core_kit/src/main/java/com/guestlogix/travelercorekit/sdk/Traveler.java
@@ -1,21 +1,21 @@
 package com.guestlogix.travelercorekit.sdk;
 
 import android.content.Context;
+import android.text.TextUtils;
 import android.util.Log;
 import com.guestlogix.TravelerLog;
 import com.guestlogix.task.*;
-import com.guestlogix.travelercorekit.models.AuthToken;
+import com.guestlogix.travelercorekit.models.Catalog;
+import com.guestlogix.travelercorekit.models.Session;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class Traveler {
 
     private static Traveler mLocalInstance;
-    private Context mContext;
-    private String mApiKey;
     private TaskManager mTaskManager = new TaskManager();
 
-    private static AuthToken mToken;
+    private Session mSession;
 
     public static void initialize(@NotNull String apiKey, @Nullable Context applicationContext) {
 
@@ -28,28 +28,101 @@ public class Traveler {
     }
 
     private Traveler(String apiKey, Context context) {
-        this.mApiKey = apiKey;
-        this.mContext = context;
+        this.mSession = new Session(apiKey, context);
 
-        // grab token from Keychain
-        // if token doesnt exist in keychain then continue
 
-        JsonObjectMapper<AuthToken> jsonObjectMapper = new JsonObjectMapper<>(new AuthToken.AuthTokenMappingFactory(), new JsonObjectMapper.Callback<AuthToken>() {
+        //read token from dist
+        SessionBeginTask sessionBeginTask = new SessionBeginTask(this.mSession);
+        //fetch token from backend if disk does not have one
+        AuthTokenFetchTask authTokenFetchTask = new AuthTokenFetchTask(this.mSession.getApiKey(), mSession.getContext());
+
+        BlockTask authTokenFetchBlockTask = new BlockTask() {
             @Override
-            public void onSuccess(AuthToken token) {
-                Log.v("Traveler", "Token:" + token.getValue());
-            }
+            protected void main() {
+                Log.v("Traveler", "Setting Session token:" + authTokenFetchTask.getToken().getValue());
 
+                mSession.setAuthToken(authTokenFetchTask.getToken());
+            }
+        };
+
+        BlockTask sessionBeginBlockTask = new BlockTask() {
             @Override
-            public void onError(Error error) {
-                Log.v("Traveler", error.getMessage());
+            protected void main() {
+                if (TextUtils.isEmpty(sessionBeginTask.getSession().getAuthToken().getValue())) {
+                    Log.v("Traveler", "Could not Find Token on disk initiating fetch...");
+                } else {
+                    Log.v("Traveler", "Found Token on disk: " + sessionBeginTask.getSession().getAuthToken().getValue());
+                    Log.v("Traveler", "Cancelling fetch token tasks");
+
+                    authTokenFetchTask.cancel();
+                    authTokenFetchBlockTask.cancel();
+                }
             }
-        });
+        };
 
-        Task networkTask = new NetworkTask(Router.authenticate(apiKey, context), jsonObjectMapper);
+        sessionBeginBlockTask.addDependency(sessionBeginTask);
+        authTokenFetchTask.addDependency(sessionBeginBlockTask);
+        authTokenFetchBlockTask.addDependency(authTokenFetchTask);
 
-        mTaskManager.addTask(networkTask);
+        mTaskManager.addTask(sessionBeginTask);
+        mTaskManager.addTask(sessionBeginBlockTask);
+        mTaskManager.addTask(authTokenFetchTask);
+        mTaskManager.addTask(authTokenFetchBlockTask);
 
     }
+
+
+//    public static void getCatalog(String id, CatalogResponseHandler catalogResponseHandler) {
+//        if (null == mLocalInstance) {
+//            catalogResponseHandler.onError(new Error("SDK not initialized, Initialize by calling Traveler.initialize(); "));
+//        } else {
+//            mLocalInstance.getCatalog(id, new JsonObjectMapper<>(new Catalog.CatalogMappingFactory(), new JsonObjectMapper.Callback<Catalog>() {
+//                @Override
+//                public void onSuccess(Catalog catalog) {
+//                    Log.v("Traveler", "Fetched  Catalog");
+//                    catalogResponseHandler.onSuccess(catalog);
+//                }
+//
+//                @Override
+//                public void onError(Error error) {
+//                    catalogResponseHandler.onError(error);
+//                    //In case of authentication error fetch auth token and re run this call
+//
+//                    if ("Authentication Error".equals(error.getMessage())) {
+//
+//                        //TODO: Look if we can chain or group tasks
+//                        //mTaskManager.addTask();
+//
+//                    } else {
+//
+//                        Log.v("Traveler", error.getMessage());
+//                        catalogResponseHandler.onError(error);
+//                    }
+//                }
+//            }));
+//        }
+//    }
+//
+//    private void getCatalog(String id, NetworkTask.ResponseHandler responseHandler) {
+//
+//
+//        BlockTask getCatalogRequestBlockTask = new BlockTask() {
+//            @Override
+//            protected void main() {
+//
+//            }
+//        };
+//
+//        Router.getCatalogue(mSession, id);
+//        AuthenticatedNetworkRequestTask getCatalogRequestTask = new AuthenticatedNetworkRequestTask();
+//
+//    }
+//
+//
+//    public interface CatalogResponseHandler {
+//        void onSuccess(Catalog catalog);
+//
+//        void onError(Error e);
+//    }
 
 }
