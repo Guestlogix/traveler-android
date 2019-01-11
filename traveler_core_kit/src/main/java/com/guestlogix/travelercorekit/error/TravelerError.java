3aab4a089db57551269762c4839d109a787c2e5a
Skrinikov <skrinikovd@gmail.com>
2019-01-11 09:56:38 -0500

    #11 Add error equals code

--- traveler_core_kit/src/main/java/com/guestlogix/travelercorekit/error/TravelerError.java
+++ traveler_core_kit/src/main/java/com/guestlogix/travelercorekit/error/TravelerError.java
@@ -16,4 +16,20 @@ public class TravelerError {
     public String toString() {
         return mCode + mMessage;
     }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final TravelerError error = (TravelerError) obj;
+
+        return this.mCode == error.getCode();
+    }
 }
