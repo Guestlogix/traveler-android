4d3dc506fcbd5c7f8121e99c6a8558b04864867a
Sayyam Mehmood <sayyam.mehmood@gmail.com>
2019-01-11 10:54:07 -0500

    Integrates app center for analytics and crashes

--- traveler_app/src/main/java/com/guestlogix/traveler/application/TravelerApplication.java
+++ traveler_app/src/main/java/com/guestlogix/traveler/application/TravelerApplication.java
@@ -2,6 +2,9 @@ package com.guestlogix.traveler.application;
 
 import android.app.Application;
 import com.guestlogix.travelercorekit.Traveler;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 public class TravelerApplication extends Application {
 
