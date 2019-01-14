841749bbf2a1c4a322f3ba147b7989922619ede8
Skrinikov <skrinikovd@gmail.com>
2019-01-14 13:55:51 -0500

    #101 Fixes double tap and include submit date from soft input.

--- traveler_app/src/main/java/com/guestlogix/traveler/fragments/FlightSearchFragment.java
+++ traveler_app/src/main/java/com/guestlogix/traveler/fragments/FlightSearchFragment.java
@@ -1,6 +1,8 @@
 package com.guestlogix.traveler.fragments;
 
 import android.app.DatePickerDialog;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
 import androidx.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
@@ -57,25 +59,11 @@ public class FlightSearchFragment extends Fragment {
         mView = inflater.inflate(R.layout.fragment_flight_search, container, false);
         ButterKnife.bind(this, mView);
 
-        departureDateEditText.setShowSoftInputOnFocus(false);
-        departureDateEditText.setOnClickListener(departureDateOnClickListener);
+        flightNumberEditText.setOnFocusChangeListener(this::flightNumberFocusHandler);
+        departureDateEditText.setOnFocusChangeListener(this::departureDateFocusHandler);
+        departureDateEditText.setOnEditorActionListener(this::softInputSubmit);
+        searchFlightsButton.setOnClickListener(this::navigateToFlightSearchResults);
 
-        flightNumberEditText.setOnFocusChangeListener((view, focus) -> {
-            if (!focus) {
-                String flightNumber = ((TextView) view).getText().toString();
-
-                validateFlightNumber(flightNumber);
-            }
-        });
-
-        departureDateEditText.setOnFocusChangeListener((view, focus) -> {
-            if (!focus) {
-                String date = ((TextView) view).getText().toString();
-
-                validateFlightDate(date);
-            }
-        });
-        searchFlightsButton.setOnClickListener(searchFlightOnClickListener);
         return mView;
     }
 
@@ -85,9 +73,7 @@ public class FlightSearchFragment extends Fragment {
         mViewModel = ViewModelProviders.of(getActivity()).get(FlightSearchViewModel.class);
     }
 
-    View.OnClickListener searchFlightOnClickListener = v -> navigateToFlightSearchResults();
-
-    private void navigateToFlightSearchResults() {
+    private void navigateToFlightSearchResults(View view) {
         String flightNumber = flightNumberEditText.getText().toString();
         String departureDate = DateHelper.getDateAsString(myCalendar.getTime());
 
@@ -99,16 +85,6 @@ public class FlightSearchFragment extends Fragment {
             validateFlightDate(departureDate);
         }
     }
-
-    View.OnClickListener departureDateOnClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-
-            new DatePickerDialog(getActivity(), date, myCalendar
-                    .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
-                    myCalendar.get(Calendar.DAY_OF_MONTH)).show();
-        }
-    };
 
     DatePickerDialog.OnDateSetListener date = (view, year, monthOfYear, dayOfMonth) -> {
         myCalendar.set(Calendar.YEAR, year);
@@ -140,4 +116,33 @@ public class FlightSearchFragment extends Fragment {
             departureDateEditText.setError(getString(R.string.empty_date_error));
         }
     }
+
+    private void flightNumberFocusHandler(View view, boolean focus) {
+        if (!focus) {
+            String flightNumber = ((TextView) view).getText().toString();
+
+            validateFlightNumber(flightNumber);
+        }
+    }
+
+    private void departureDateFocusHandler(View view, boolean focus) {
+        if (!focus) {
+            String date = ((TextView) view).getText().toString();
+
+            validateFlightDate(date);
+        } else {
+            new DatePickerDialog(getActivity(), date, myCalendar
+                    .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                    myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+        }
+    }
+
+    private boolean softInputSubmit(View v, int actionId, KeyEvent event) {
+        if (EditorInfo.IME_ACTION_DONE == actionId) {
+            searchFlightsButton.performClick();
+            return true;
+        }
+        return false;
+    }
+
 }
