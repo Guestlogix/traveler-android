316e48a317b63aa9199c1063cf66142af6e9c97e
Sayyam Mehmood <sayyam.mehmood@gmail.com>
2019-01-25 10:03:45 -0500

    adds spinner adapter for time slots

---
+++ traveler_ui_kit/src/main/java/com/guestlogix/traveleruikit/adapters/TimeSlotSpinnerAdapter.java
@@ -0,0 +1,27 @@
+package com.guestlogix.traveleruikit.adapters;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import com.guestlogix.travelercorekit.utilities.DateHelper;
+
+import java.util.List;
+
+public class TimeSlotSpinnerAdapter extends ArrayAdapter<Long> {
+
+    public TimeSlotSpinnerAdapter(Activity context, int resourceId, @NonNull List<Long> list) {
+        super(context, resourceId, list);
+    }
+
+    @Override
+    public View getDropDownView(int position, View convertView,
+                                ViewGroup parent) {
+        TextView label = (TextView) super.getDropDownView(position, convertView, parent);
+        label.setText(DateHelper.getDayMinutesAsTimeString(getItem(position)));
+
+        return label;
+    }
+}
