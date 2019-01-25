fd4819e83f0a2d1b87ea93cfcf232b29b4211e7b
Sayyam Mehmood <sayyam.mehmood@gmail.com>
2019-01-25 10:05:09 -0500

    updates date helper to convert minutes to pretty time

--- traveler_core_kit/src/main/java/com/guestlogix/travelercorekit/utilities/DateHelper.java
+++ traveler_core_kit/src/main/java/com/guestlogix/travelercorekit/utilities/DateHelper.java
@@ -2,13 +2,15 @@ package com.guestlogix.travelercorekit.utilities;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
 public class DateHelper {
-    public static final String DATE_PATTERN = "yyyy-MM-dd'T'HH:mm:ss";
-    public static final String PRETTY_DATE_PATTERN = "dd MMM yyyy";
-    public static final String TIME_PATTERN = "HH:mm";
+    private static final String DATE_PATTERN = "yyyy-MM-dd'T'HH:mm:ss";
+    private static final String PRETTY_DATE_PATTERN = "dd MMM yyyy";
+    private static final String TIME_PATTERN = "HH:mm";
+    private static final Calendar mCalendar = Calendar.getInstance();
 
     public static String getDateAsString(Date date) {
         return new SimpleDateFormat(DATE_PATTERN, Locale.US).format(date);
@@ -24,5 +26,13 @@ public class DateHelper {
 
     public static String getTimeAsString(Date date) {
         return new SimpleDateFormat(TIME_PATTERN, Locale.US).format(date);
+    }
+
+    public static String getDayMinutesAsTimeString(Long rowItem) {
+        if (null == rowItem)
+            return null;
+        mCalendar.set(Calendar.HOUR_OF_DAY, rowItem.intValue() / 60);
+        mCalendar.set(Calendar.MINUTE, rowItem.intValue() % 60);
+        return DateHelper.getTimeAsString(mCalendar.getTime());
     }
 }
