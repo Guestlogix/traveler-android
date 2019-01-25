2e984a695a67d1c29efef0ea1f9b758d689c1156
Skrinikov <skrinikovd@gmail.com>
2019-01-25 15:07:59 -0500

    Adds Pass model and mapping from Json

---
+++ traveler_core_kit/src/main/java/com/guestlogix/travelercorekit/models/Pass.java
@@ -0,0 +1,110 @@
+package com.guestlogix.travelercorekit.models;
+
+import android.util.JsonReader;
+import com.guestlogix.travelercorekit.network.ObjectMappingException;
+import com.guestlogix.travelercorekit.network.ObjectMappingFactory;
+import com.guestlogix.travelercorekit.utilities.JsonReaderHelper;
+
+import java.io.IOException;
+
+public class Pass {
+    private String id;
+    private String name;
+    private String description;
+    private Integer maxQuantity;
+    private Double price;
+
+    public Pass(String id, String name, String description, Integer maxQuantity, Double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.maxQuantity = maxQuantity;
+        this.price = price;
+    }
+
+    public int getMaxQuantity() {
+        return maxQuantity;
+    }
+
+    public void setMaxQuantity(int maxQuantity) {
+        this.maxQuantity = maxQuantity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public static class PassObjectMappingFactory implements ObjectMappingFactory<Pass> {
+
+        @Override
+        public Pass instantiate(JsonReader reader) throws ObjectMappingException, IOException {
+            return readPass(reader);
+        }
+
+        private static Pass readPass(JsonReader reader) throws IOException {
+            String id = "";
+            String name = "";
+            String description = "";
+            Integer maxQuantity = 0;
+            Double price = 0.0;
+
+            reader.beginObject();
+
+            while (reader.hasNext()) {
+                String key = reader.nextName();
+
+                switch (key) {
+                    case "id":
+                        id = JsonReaderHelper.readString(reader);
+                        break;
+                    case "name":
+                        name = JsonReaderHelper.readString(reader);
+                        break;
+                    case "description":
+                        description = JsonReaderHelper.readString(reader);
+                        break;
+                    case "price":
+                        price = JsonReaderHelper.readDouble(reader);
+                        break;
+                    case "maximumQuantity":
+                        maxQuantity = JsonReaderHelper.readInteger(reader);
+                        break;
+                    default:
+                            reader.skipValue();
+                }
+            }
+
+            reader.endObject();
+
+            return new Pass(id, name, description, maxQuantity, price);
+        }
+    }
+}
