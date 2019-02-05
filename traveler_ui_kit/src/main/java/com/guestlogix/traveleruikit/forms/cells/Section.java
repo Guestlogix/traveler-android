df8090d01114d26986aecfe6a188802c8ad7968b
Skrinikov <skrinikovd@gmail.com>
2019-02-05 15:51:08 -0500

    Adds callbacks to the library

--- traveler_ui_kit/src/main/java/com/guestlogix/traveleruikit/forms/cells/Section.java
+++ traveler_ui_kit/src/main/java/com/guestlogix/traveleruikit/forms/cells/Section.java
@@ -2,25 +2,9 @@ package com.guestlogix.traveleruikit.forms.cells;
 
 import android.view.View;
 import androidx.annotation.NonNull;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Section extends FormCell {
-    private List<Input> inputs;
-
     public Section(@NonNull View itemView) {
         super(itemView);
-    }
-
-    public void setInputs(List<Input> inputs) {
-        this.inputs = inputs;
-    }
-
-    public void addInput(Input input) {
-        if (null == inputs) {
-            inputs = new ArrayList<>();
-        }
-
-        inputs.add(input);
     }
 }
