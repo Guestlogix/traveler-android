cdd9bffd5c423d56d706d1ec55983355314acae8
Sayyam Mehmood <sayyam.mehmood@gmail.com>
2019-02-05 14:05:04 -0500

    adds traveler loading fragment

---
+++ traveler_ui_kit/src/main/java/com/guestlogix/traveleruikit/fragments/TravelerLoadingFragment.java
@@ -0,0 +1,30 @@
+package com.guestlogix.traveleruikit.fragments;
+
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.guestlogix.traveleruikit.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TravelerLoadingFragment extends Fragment {
+
+
+    public TravelerLoadingFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_traveler_loading, container, false);
+    }
+
+}
