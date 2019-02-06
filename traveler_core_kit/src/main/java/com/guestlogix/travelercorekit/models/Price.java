147ec3abe7b4364e39deadf507b01c9962e6f8aa
Sayyam Mehmood <sayyam.mehmood@gmail.com>
2019-02-06 16:17:15 -0500

    adds price model

---
+++ traveler_core_kit/src/main/java/com/guestlogix/travelercorekit/models/Price.java
@@ -0,0 +1,67 @@
+package com.guestlogix.travelercorekit.models;
+
+import android.util.JsonReader;
+import android.util.Log;
+import com.guestlogix.travelercorekit.network.ObjectMappingFactory;
+import com.guestlogix.travelercorekit.utilities.JsonReaderHelper;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+public class Price implements Serializable {
+
+    Double value;
+    String currency;
+
+    public Price() {
+    }
+
+    public Price(Double value, String currency) {
+        this.value = value;
+        this.currency = currency;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public static class PriceObjectMappingFactory implements ObjectMappingFactory<Price> {
+
+        @Override
+        public Price instantiate(JsonReader reader) throws IOException {
+            return readItem(reader);
+        }
+
+        private Price readItem(JsonReader reader) throws IOException {
+            Double value = 0.0;
+            String currency = "";
+
+            reader.beginObject();
+            while (reader.hasNext()) {
+                String name = reader.nextName();
+                Log.d("Price", "Key:" + name);
+                switch (name) {
+                    case "value":
+                        value = JsonReaderHelper.readDouble(reader);
+                        break;
+                    case "currency":
+                        currency = JsonReaderHelper.readString(reader);
+                        break;
+                }
+            }
+            return new Price(value, currency);
+        }
+    }
+}
