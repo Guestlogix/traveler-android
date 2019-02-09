f03f0e923bf634030da4275e641504dd4cc53ebe
Sayyam Mehmood <sayyam.mehmood@gmail.com>
2019-02-08 20:41:11 -0500

    adds catalog widget viewmodel

---
+++ traveler_ui_kit/src/main/java/com/guestlogix/traveleruikit/viewmodels/CatalogWidgetViewModel.java
@@ -0,0 +1,59 @@
+package com.guestlogix.traveleruikit.viewmodels;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+import com.guestlogix.travelercorekit.callbacks.CatalogSearchCallback;
+import com.guestlogix.travelercorekit.error.TravelerError;
+import com.guestlogix.travelercorekit.models.Catalog;
+import com.guestlogix.travelercorekit.models.CatalogGroup;
+import com.guestlogix.travelercorekit.models.CatalogQuery;
+import com.guestlogix.traveleruikit.repositories.CatalogWidgetRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogWidgetViewModel extends ViewModel {
+
+    private CatalogWidgetRepository catalogWidgetRepository;
+    private MutableLiveData<List<CatalogGroup>> catalogGroupList;
+    private MutableLiveData<CatalogWidgetViewState> viewState = new MutableLiveData<>();
+
+    public MutableLiveData<CatalogWidgetViewState> getViewStateObservable() {
+        return viewState;
+    }
+
+    public LiveData<List<CatalogGroup>> getGroupsObservable() {
+        return catalogGroupList;
+    }
+
+    public CatalogWidgetViewModel() {
+        catalogWidgetRepository = new CatalogWidgetRepository();
+        this.catalogGroupList = new MutableLiveData<>();
+        this.catalogGroupList.setValue(new ArrayList<>());
+    }
+
+    public void updateCatalog(CatalogQuery catalogQuery) {
+        viewState.postValue(CatalogWidgetViewState.LOADING);
+        catalogWidgetRepository.catalogSearch(catalogQuery, catalogSearchCallback);
+    }
+
+    private CatalogSearchCallback catalogSearchCallback = new CatalogSearchCallback() {
+        @Override
+        public void onCatalogSearchSuccess(Catalog catalog) {
+            catalogGroupList.postValue(catalog.getGroups());
+            viewState.postValue(CatalogWidgetViewState.SUCCESS);
+        }
+
+        @Override
+        public void onCatalogSearchError(TravelerError error) {
+            viewState.postValue(CatalogWidgetViewState.ERROR);
+        }
+    };
+
+    public enum CatalogWidgetViewState {
+        LOADING,
+        SUCCESS,
+        ERROR,
+    }
+}
