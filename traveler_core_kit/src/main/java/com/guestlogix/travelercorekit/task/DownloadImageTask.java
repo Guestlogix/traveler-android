22edda3ddc68f89af53947ef76057aaa9a282101
Sayyam Mehmood <sayyam.mehmood@gmail.com>
2019-02-13 16:46:22 -0500

    Adds task to download image

---
+++ traveler_core_kit/src/main/java/com/guestlogix/travelercorekit/task/DownloadImageTask.java
@@ -0,0 +1,49 @@
+package com.guestlogix.travelercorekit.task;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+import com.guestlogix.travelercorekit.error.TravelerError;
+
+public class DownloadImageTask extends Task {
+
+    private TaskManager mTaskManager = new TaskManager();
+    private NetworkTask.Request imageRequest;
+    private TravelerError mError;
+    private Bitmap resource;
+
+
+    public DownloadImageTask(NetworkTask.Request imageRequest) {
+        this.imageRequest = imageRequest;
+    }
+
+    @Override
+    public void execute() {
+
+        Log.v("Traveler", "DownloadImageTask execute()");
+
+        //Fetch image from backend
+        NetworkTask downloadImageNetworkTask = new NetworkTask(imageRequest, stream -> resource = BitmapFactory.decodeStream(stream));
+
+        BlockTask finishTask = new BlockTask() {
+            @Override
+            protected void main() {
+                mError = downloadImageNetworkTask.getError();
+                DownloadImageTask.this.finish();
+            }
+        };
+
+        finishTask.addDependency(downloadImageNetworkTask);
+
+        mTaskManager.addTask(downloadImageNetworkTask);
+        mTaskManager.addTask(finishTask);
+    }
+
+    public TravelerError getError() {
+        return mError;
+    }
+
+    public Bitmap getResource() {
+        return resource;
+    }
+}
