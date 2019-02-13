72d370080ebbd4b2b178ecb199b71e803016f6a0
Sayyam Mehmood <sayyam.mehmood@gmail.com>
2019-02-13 16:46:37 -0500

    Adds image request builder

---
+++ traveler_ui_kit/src/main/java/com/guestlogix/traveleruikit/tools/image/ImageRequest.java
@@ -0,0 +1,39 @@
+package com.guestlogix.traveleruikit.tools.image;
+
+import com.guestlogix.travelercorekit.task.NetworkTask;
+
+import java.io.OutputStream;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Network Request for Images
+ */
+public class ImageRequest implements NetworkTask.Request {
+    private URL url;
+
+    public ImageRequest(URL url) {
+        this.url = url;
+    }
+
+    @Override
+    public Method getMethod() {
+        return Method.GET;
+    }
+
+    @Override
+    public URL getURL() {
+        return url;
+    }
+
+    @Override
+    public Map<String, String> getHeaders() {
+        return new HashMap<>();
+    }
+
+    @Override
+    public void onProvidePayload(OutputStream stream) {
+
+    }
+}
