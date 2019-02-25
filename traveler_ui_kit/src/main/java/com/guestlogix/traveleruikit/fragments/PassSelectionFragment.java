3599d01ed7cfdfabd8bbaae2556627aa9ce77a4b
Sayyam Mehmood <sayyam.mehmood@gmail.com>
2019-02-25 17:20:14 -0500

    parses html to plain text in form

--- traveler_ui_kit/src/main/java/com/guestlogix/traveleruikit/fragments/PassSelectionFragment.java
+++ traveler_ui_kit/src/main/java/com/guestlogix/traveleruikit/fragments/PassSelectionFragment.java
@@ -10,7 +10,6 @@ import android.widget.Button;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 import com.guestlogix.travelercorekit.models.Pass;
 import com.guestlogix.traveleruikit.R;
