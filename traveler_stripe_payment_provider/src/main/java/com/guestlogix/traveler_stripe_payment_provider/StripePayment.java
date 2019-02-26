8bed02b9af7d48148e53d8452fc445f0463585fc
Sayyam Mehmood <sayyam.mehmood@gmail.com>
2019-02-26 14:29:47 -0500

    cleans up stripe payment provides module

--- traveler_stripe_payment_provider/src/main/java/com/guestlogix/traveler_stripe_payment_provider/StripePayment.java
+++ traveler_stripe_payment_provider/src/main/java/com/guestlogix/traveler_stripe_payment_provider/StripePayment.java
@@ -1,8 +1,8 @@
 package com.guestlogix.traveler_stripe_payment_provider;
 
-import android.util.Log;
 import com.guestlogix.travelercorekit.models.Attribute;
 import com.guestlogix.travelercorekit.models.Payment;
+import com.guestlogix.travelercorekit.utilities.TravelerLog;
 import com.stripe.android.model.Token;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -11,24 +11,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StripePayment implements Payment {
-    private String mTokenID;
-    private String mLastFourDigits;
-    private String mExpirationMonth;
-    private String mExpirationYear;
+    private String tokenID;
+    private String lastFourDigits;
+    private String expirationMonth;
+    private String expirationYear;
 
     public StripePayment(Token token) {
-        mTokenID = token.getId();
-        mExpirationMonth = token.getCard().getExpMonth().toString();
-        mExpirationYear = token.getCard().getExpYear().toString();
-        mLastFourDigits = token.getCard().getLast4();
+        tokenID = token.getId();
+        expirationMonth = token.getCard().getExpMonth().toString();
+        expirationYear = token.getCard().getExpYear().toString();
+        lastFourDigits = token.getCard().getLast4();
     }
 
     @Override
     public List<Attribute> getAttributes() {
         ArrayList<Attribute> attributes = new ArrayList<>();
-        attributes.add(new Attribute("Credit card number", mLastFourDigits));
+        attributes.add(new Attribute("Credit card number", lastFourDigits));
         attributes.add(new Attribute("Expiry date",
-                mExpirationMonth + "/" + mExpirationYear));
+                expirationMonth + "/" + expirationYear));
         return attributes;
     }
 
@@ -36,9 +36,9 @@ public class StripePayment implements Payment {
     public JSONObject getSecurePayload() {
         JSONObject jsonObject = new JSONObject();
         try {
-            jsonObject.put("token", mTokenID);
+            jsonObject.put("token", tokenID);
         } catch (JSONException je) {
-            Log.e("StripePayment", "Could not construct payload.");
+            TravelerLog.e("Could not construct payload.");
             je.printStackTrace();
             return null;
         }
