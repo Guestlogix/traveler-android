a500c8e854439ff7086191f4ba8fcfb7363e2b07
Skrinikov <skrinikovd@gmail.com>
2019-03-08 13:30:51 -0500

    Fixes deployment scripts

--- .deployment/build-doc.sh
+++ .deployment/build-doc.sh
@@ -5,16 +5,18 @@ echo "Creating an aggregate directory"
 rm -rf Javadoc 
 mkdir JavaDocDir
 mkdir JavaDoc
+
+# Add or remove any extra files for the docs here.
+
 echo "Copying travelercorekit packages..."
-cp -r ../traveler_core_kit/src/main/java/com/guestlogix/travelercorekit/models ./JavaDocDir
-cp -r ../traveler_core_kit/src/main/java/com/guestlogix/travelercorekit/callbacks ./JavaDocDir 
+cp -r traveler_core_kit/src/main/java/com/guestlogix/travelercorekit/models ./JavaDocDir
+cp -r traveler_core_kit/src/main/java/com/guestlogix/travelercorekit/callbacks ./JavaDocDir 
 
 echo "Copying traveleruikit packages..."
-cp -r ../traveler_ui_kit/src/main/java/com/guestlogix/traveleruikit/forms ./JavaDocDir
-cp -r ../traveler_ui_kit/src/main/java/com/guestlogix/traveleruikit/widgets ./JavaDocDir
+cp -r traveler_ui_kit/src/main/java/com/guestlogix/traveleruikit/forms ./JavaDocDir
+cp -r traveler_ui_kit/src/main/java/com/guestlogix/traveleruikit/widgets ./JavaDocDir
 
 echo "Done copying Packages! Generating JavaDocs"
+# For some reason the Javadoc command does not recursively look for package names. Add folders you want to see manually.
 javadoc -d JavaDoc JavaDocDir/models/*.java JavaDocDir/callbacks/*.java JavaDocDir/forms/*.java JavaDocDir/forms/cells/*.java JavaDocDir/forms/descriptors/*.java JavaDocDir/widgets/*.java
-tar -zcvf docs.gz JavaDoc/
 rm -r JavaDocDir
-rm -r JavaDoc
