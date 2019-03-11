150b0550455b1afba7942cb358b3175233894777
Skrinikov <skrinikovd@gmail.com>
2019-03-11 16:02:44 -0400

    Changes integration to use s3cmd
    Adds cmds3
    
    Change wget to brew
    
    Test using s3
    
    fsdf
    
    Should upload to s3
    
    pls work

--- traveler_app/appcenter-post-build.sh
+++ traveler_app/appcenter-post-build.sh
@@ -5,4 +5,10 @@ mkdir Documentation
 javadoc -d Documentation ../traveler_core_kit/src/main/java/com/guestlogix/travelercorekit/models/*.java ../traveler_core_kit/src/main/java/com/guestlogix/travelercorekit/callbacks/*.java ../traveler_ui_kit/src/main/java/com/guestlogix/traveleruikit/activities/*.java ../traveler_ui_kit/src/main/java/com/guestlogix/traveleruikit/fragments/*.java
 
 # Do something with the docs.
-echo "Test Test Test"
+brew install s3cmd
+
+s3Bucket="s3://$S3_BUCKET_ANDROID"
+s3cmd --access_key=$AWS_ACCESS_KEY
+s3cmd --secret_key=$AWS_SECRET_ACCESS_KEY
+s3cmd --region=$REGION
+s3cmd put --delete-removed --recursive --acl-public ./Documentation/ $s3Bucket --region=$REGION --access_key=$AWS_ACCESS_KEY --secret_key=$AWS_SECRET_ACCESS_KEY
