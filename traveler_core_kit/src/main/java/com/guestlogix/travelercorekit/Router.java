e62e95a828493adfdf0a10bcc9b707c250192bca
Sayyam Mehmood <sayyam.mehmood@gmail.com>
2019-03-22 14:38:51 -0400

    refactors resources

--- traveler_core_kit/src/main/java/com/guestlogix/travelercorekit/Router.java
+++ traveler_core_kit/src/main/java/com/guestlogix/travelercorekit/Router.java
@@ -21,7 +21,7 @@ import static com.guestlogix.travelercorekit.utilities.UrlHelper.urlEncodeUTF8;
 
 
 public class Router {
-    private static final String BASE_URL = "https://guest-traveler-api-livn.guestlogix.io";
+    private static final String BASE_URL = "https://guest-traveler-api-develop.guestlogix.io";
 
     public static UnauthenticatedUrlRequest authenticate(String apiKey, Context context) {
         return RequestBuilder.Builder()
