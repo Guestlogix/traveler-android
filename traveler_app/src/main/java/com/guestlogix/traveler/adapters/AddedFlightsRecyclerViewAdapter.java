064742d6b3a0ec3a77c82a0618a1d3553daa0112
Skrinikov <skrinikovd@gmail.com>
2019-04-01 10:58:25 -0400

    Disables flight deletion while the catalog is being fetched.

--- traveler_app/src/main/java/com/guestlogix/traveler/adapters/AddedFlightsRecyclerViewAdapter.java
+++ traveler_app/src/main/java/com/guestlogix/traveler/adapters/AddedFlightsRecyclerViewAdapter.java
@@ -3,7 +3,9 @@ package com.guestlogix.traveler.adapters;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import com.guestlogix.traveler.R;
 import com.guestlogix.travelercorekit.models.Flight;
@@ -12,26 +14,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class HomeFragmentRecyclerViewAdapter extends RecyclerView.Adapter<HomeFragmentRecyclerViewAdapter.ViewHolder> {
+public class AddedFlightsRecyclerViewAdapter extends RecyclerView.Adapter<AddedFlightsRecyclerViewAdapter.ViewHolder> {
 
     private List<Flight> flightList = new ArrayList<>();
     private View.OnClickListener deleteFlightOnClickListener;
 
+    @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.fragment_home_flight_item, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(final ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
         holder.item = flightList.get(position);
 
-        holder.departureIataTextView.setText(flightList.get(position).getDepartureAirport().getCode());
-        holder.arrivalIataTextView.setText(flightList.get(position).getArrivalAirport().getCode());
-        holder.deleteTextView.setOnClickListener(deleteFlightOnClickListener);
-        holder.deleteTextView.setTag(position);
+        holder.departureIata.setText(holder.item.getDepartureAirport().getCode());
+        holder.arrivalIata.setText(holder.item.getArrivalAirport().getCode());
+        holder.deleteFlight.setOnClickListener(deleteFlightOnClickListener);
+        holder.deleteFlight.setTag(position);
     }
 
     @Override
@@ -39,9 +42,9 @@ public class HomeFragmentRecyclerViewAdapter extends RecyclerView.Adapter<HomeFr
         return flightList.size();
     }
 
-    public void update(List<Flight> flightsArrayList) {
+    public void update(List<Flight> flights) {
         flightList.clear();
-        flightList.addAll(flightsArrayList);
+        flightList.addAll(flights);
         notifyDataSetChanged();
     }
 
@@ -51,18 +54,18 @@ public class HomeFragmentRecyclerViewAdapter extends RecyclerView.Adapter<HomeFr
 
     class ViewHolder extends RecyclerView.ViewHolder {
         final View view;
-        TextView departureIataTextView;
-        TextView arrivalIataTextView;
-        TextView deleteTextView;
+        TextView departureIata;
+        TextView arrivalIata;
+        Button deleteFlight;
         Flight item;
 
         ViewHolder(View view) {
             super(view);
             this.view = view;
 
-            departureIataTextView = view.findViewById(R.id.departureIataTextView);
-            arrivalIataTextView = view.findViewById(R.id.arrivalIataTextView);
-            deleteTextView = view.findViewById(R.id.deleteTextView);
+            departureIata = view.findViewById(R.id.textView_catalog_departureIata);
+            arrivalIata = view.findViewById(R.id.textView_catalog_arrivalIata);
+            deleteFlight = view.findViewById(R.id.button_catalog_deleteFlights);
         }
     }
 }
--- traveler_app/src/main/java/com/guestlogix/traveler/fragments/CatalogFragment.java
+++ traveler_app/src/main/java/com/guestlogix/traveler/fragments/CatalogFragment.java
@@ -4,13 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.guestlogix.traveler.R;
-import com.guestlogix.traveler.adapters.HomeFragmentRecyclerViewAdapter;
+import com.guestlogix.traveler.adapters.AddedFlightsRecyclerViewAdapter;
 import com.guestlogix.traveler.viewmodels.CatalogViewModel;
 import com.guestlogix.travelercorekit.models.Flight;
 import com.guestlogix.traveleruikit.fragments.BaseFragment;
@@ -18,11 +19,12 @@ import com.guestlogix.traveleruikit.fragments.CatalogWidgetFragment;
 
 import java.util.List;
 
-public class CatalogFragment extends BaseFragment {
+public class CatalogFragment extends BaseFragment implements CatalogWidgetFragment.CatalogFetchSuccessCallback {
 
     private CatalogWidgetFragment catalogWidgetFragment;
     private CatalogViewModel catalogViewModel;
-    private HomeFragmentRecyclerViewAdapter homeFragmentRecyclerViewAdapter;
+    private AddedFlightsRecyclerViewAdapter adapter;
+    private boolean canDeleteFlight = true;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -30,18 +32,21 @@ public class CatalogFragment extends BaseFragment {
         View catalogFragmentView = inflater.inflate(R.layout.fragment_catalog, container, false);
 
         catalogWidgetFragment = (CatalogWidgetFragment) getChildFragmentManager().findFragmentById(R.id.fragment_catalogFragment_catalogContainer);
+        if (catalogWidgetFragment != null) {
+            catalogWidgetFragment.setCatalogFetchSuccessCallback(this);
+        }
 
         // Setup flights recycler view.
         RecyclerView flightResultRecyclerView = catalogFragmentView.findViewById(R.id.recyclerView_catalogFragment_addedFlights);
         LinearLayoutManager layoutManager = new LinearLayoutManager(flightResultRecyclerView.getContext());
-        homeFragmentRecyclerViewAdapter = new HomeFragmentRecyclerViewAdapter();
-        homeFragmentRecyclerViewAdapter.setDeleteFlightOnClickListener(deleteFlightOnClickListener);
+        adapter = new AddedFlightsRecyclerViewAdapter();
+        adapter.setDeleteFlightOnClickListener(this::onDeleteClick);
         DividerItemDecoration decorator = new DividerItemDecoration
                 (flightResultRecyclerView.getContext(), layoutManager.getOrientation());
 
         // Bind recycler view data.
         flightResultRecyclerView.setLayoutManager(layoutManager);
-        flightResultRecyclerView.setAdapter(homeFragmentRecyclerViewAdapter);
+        flightResultRecyclerView.setAdapter(adapter);
         flightResultRecyclerView.addItemDecoration(decorator);
 
         return catalogFragmentView;
@@ -54,20 +59,23 @@ public class CatalogFragment extends BaseFragment {
         catalogViewModel.getObservableFlights().observe(getActivityContext(), this::flightsUpdateHandler);
     }
 
-    private void updateCatalog(List<Flight> flights) {
-        catalogWidgetFragment.updateCatalog(flights);
+    @Override
+    public void onCatalogFetchSuccess() {
+        // Enable deleting flights
+        canDeleteFlight = true;
     }
 
-    View.OnClickListener deleteFlightOnClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
+    // TODO: This is not a good solution for a UX stand point. Find a better strategy!
+    private void onDeleteClick(View v) {
+        if (canDeleteFlight) {
             int index = (Integer) v.getTag();
             catalogViewModel.deleteFlight(index);
         }
-    };
+    }
 
     private void flightsUpdateHandler(List<Flight> flights) {
-        updateCatalog(flights);
-        homeFragmentRecyclerViewAdapter.update(flights);
+        canDeleteFlight = false;
+        catalogWidgetFragment.updateCatalog(flights);
+        adapter.update(flights);
     }
 }
