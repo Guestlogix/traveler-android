64b9e1e8b6bfbcec982b3f8db3188ed1b8c39c15
Skrinikov <skrinikovd@gmail.com>
2019-04-08 09:29:57 -0400

    Modifies CatalogItemDetails purchase fragments to avoid ViewModels

--- traveler_ui_kit/src/main/java/com/guestlogix/traveleruikit/fragments/BookableActionStripFragment.java
+++ traveler_ui_kit/src/main/java/com/guestlogix/traveleruikit/fragments/BookableActionStripFragment.java
@@ -7,81 +7,122 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.appcompat.app.AlertDialog;
 import com.guestlogix.travelercorekit.TravelerLog;
+import com.guestlogix.travelercorekit.callbacks.FetchPassesCallback;
+import com.guestlogix.travelercorekit.models.Pass;
+import com.guestlogix.travelercorekit.models.Price;
 import com.guestlogix.travelercorekit.models.Product;
+import com.guestlogix.travelercorekit.models.Traveler;
 import com.guestlogix.traveleruikit.R;
 import com.guestlogix.traveleruikit.activities.PassSelectionActivity;
-import com.guestlogix.traveleruikit.viewmodels.BookableProductViewModel;
+import com.guestlogix.traveleruikit.models.BookingContext;
+import com.guestlogix.traveleruikit.models.PurchaseContext;
 import com.guestlogix.traveleruikit.widgets.ActionStrip;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.Locale;
 
 /**
  * Fragment to handle Bookable item actions
  */
-public class BookableActionStripFragment extends BaseFragment {
+public class BookableActionStripFragment extends PurchaseFragment implements FetchPassesCallback {
+    public static final String BOOKING_CONTEXT_ARG = "arg_booking_context";
 
+    // Views
     private ActionStrip actionStrip;
-    private BookableProductViewModel sharedViewModel;
+
+    // Data
+    private BookingContext bookingContext;
 
     public BookableActionStripFragment() {
-        // Required empty public constructor
+        // Do nothing.
     }
 
+    public static BookableActionStripFragment getInstance(BookingContext bookingContext) {
+        BookableActionStripFragment f = new BookableActionStripFragment();
+        Bundle args = new Bundle();
+        args.putSerializable(BOOKING_CONTEXT_ARG, bookingContext);
+        f.setArguments(args);
+        return f;
+    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_bookable_action_strip, container, false);
+        Bundle args = getArguments();
+
+        View view = inflater.inflate(R.layout.fragment_bookable_action_strip, container, false);
+        actionStrip = view.findViewById(R.id.action_container);
+        actionStrip.changeState(ActionStrip.ActionStripState.DISABLED);
+        actionStrip.setActionOnClickListener(this::onActionStripClick);
+
+        if (args != null && args.containsKey(BOOKING_CONTEXT_ARG)) {
+            bookingContext = (BookingContext) args.getSerializable(BOOKING_CONTEXT_ARG);
+            setup();
+        }
+
+        return view;
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    void setPurchaseContext(PurchaseContext purchaseContext) {
+        bookingContext = (BookingContext) purchaseContext;
+        setup();
+    }
 
-        sharedViewModel = ViewModelProviders.of(getActivityContext()).get(BookableProductViewModel.class);
-        sharedViewModel.getAvailabilityState().observe(this, (state -> {
-            switch (state) {
-                case LOADING:
-                    actionStrip.changeState(ActionStrip.ActionStripState.LOADING);
-                    break;
-                case AVAILABLE:
-                    actionStrip.changeState(ActionStrip.ActionStripState.ENABLED);
-                    break;
-                case OPTION_NEEDED:
-                case NOT_AVAILABLE:
-                case ERROR:
-                case DEFAULT:
-                    actionStrip.changeState(ActionStrip.ActionStripState.DISABLED);
-                    break;
-                default:
-                    TravelerLog.w("State not Handled: %s", state.toString());
-                    break;
-            }
-        }));
+    private void setup() {
+        // Set Price
+        Price price = bookingContext.getPrice();
+        String checkAvailability = getActivityContext().getString(R.string.button_check_availability);
+        String startingAt = getActivityContext().getString(R.string.label_starting_at);
+        String localizedPrice = String.format(Locale.getDefault(), getActivityContext().getString(R.string.label_price_per_person), price.getFormattedValue());
 
-        sharedViewModel.getObservablePasses().observe(this, passes -> {
-            Product p = sharedViewModel.getProduct();
+        actionStrip.setStripValues(checkAvailability, startingAt, localizedPrice);
 
-            Intent i = new Intent(getActivityContext(), PassSelectionActivity.class);
-            i.putExtra(PassSelectionActivity.EXTRA_PRODUCT, p);
-            i.putExtra(PassSelectionActivity.EXTRA_PASSES, (Serializable) passes);
-            startActivity(i);
-        });
+        // Set state
+        PurchaseContext.State state = bookingContext.getState();
+        switch (state) {
+            case LOADING:
+                actionStrip.changeState(ActionStrip.ActionStripState.LOADING);
+                break;
+            case AVAILABLE:
+                actionStrip.changeState(ActionStrip.ActionStripState.ENABLED);
+                break;
+            case OPTION_REQUIRED:
+            case NOT_AVAILABLE:
+            case DEFAULT:
+                actionStrip.changeState(ActionStrip.ActionStripState.DISABLED);
+                break;
+            default:
+                TravelerLog.w("State not Handled: %s", state.toString());
+                break;
+        }
+    }
 
-        actionStrip = view.findViewById(R.id.action_container);
-        actionStrip.changeState(ActionStrip.ActionStripState.DISABLED);
+    // Submit traveler.
+    private void onActionStripClick(View _view) {
+        actionStrip.changeState(ActionStrip.ActionStripState.LOADING);
+        Traveler.fetchPasses(bookingContext.getProduct(), bookingContext.getAvailability(), bookingContext.getOption(), this);
+    }
 
-        sharedViewModel.getObservablePrice().observe(this, (price -> {
-            String checkAvailability = getString(R.string.button_check_availability);
-            String startingAt = getString(R.string.label_starting_at);
-            String localizedPrice = String.format(Locale.getDefault(), getString(R.string.label_price_per_person), price.getFormattedValue());
+    @Override
+    public void onPassFetchSuccess(List<Pass> pass) {
+        actionStrip.changeState(ActionStrip.ActionStripState.ENABLED);
+        Product p = bookingContext.getProduct();
 
-            actionStrip.setStripValues(checkAvailability, startingAt, localizedPrice);
-        }));
+        Intent i = new Intent(getActivityContext(), PassSelectionActivity.class);
+        i.putExtra(PassSelectionActivity.EXTRA_PRODUCT, p);
+        i.putExtra(PassSelectionActivity.EXTRA_PASSES, (Serializable) pass);
+        startActivity(i);
+    }
 
-        actionStrip.setActionOnClickListener(v -> sharedViewModel.submit());
+    @Override
+    public void onPassFetchError(Error error) {
+        actionStrip.changeState(ActionStrip.ActionStripState.ENABLED);
+        new AlertDialog.Builder(getActivityContext())
+                .setTitle(R.string.unexpected_error)
+                .setMessage(error.getMessage())
+                .show();
     }
 }
---
+++ traveler_ui_kit/src/main/java/com/guestlogix/traveleruikit/fragments/PurchaseFragment.java
@@ -0,0 +1,7 @@
+package com.guestlogix.traveleruikit.fragments;
+
+import com.guestlogix.traveleruikit.models.PurchaseContext;
+
+public abstract class PurchaseFragment extends BaseFragment {
+    abstract void setPurchaseContext(PurchaseContext purchaseContext);
+}
