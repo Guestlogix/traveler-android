f481b453b6a27f02bab7579dc9ad14018cb72665
Alvin Fong <aktfong@gmail.com>
2019-09-26 15:01:05 -0400

    feat(core): adds test for add wishlist to TravelerTest.

--- traveler_core_kit/src/main/java/com/guestlogix/travelercorekit/tasks/NetworkTaskError.java
+++ traveler_core_kit/src/main/java/com/guestlogix/travelercorekit/tasks/NetworkTaskError.java
@@ -2,12 +2,13 @@ package com.guestlogix.travelercorekit.tasks;
 
 public class NetworkTaskError extends Error {
     public enum Code {
-        BAD_URL, CONNECTION_ERROR, NO_ROUTE, FORBIDDEN, UNAUTHORIZED, SERVER_ERROR, CLIENT_ERROR
+        BAD_URL, CONNECTION_ERROR, NO_ROUTE, FORBIDDEN, UNAUTHORIZED, SERVER_ERROR, CLIENT_ERROR, ALREADY_WISHLISTED,
+        ITEM_UNAVAILABLE
     }
 
     private Code code;
 
-    NetworkTaskError(Code code) {
+    public NetworkTaskError(Code code) {
         super();
 
         this.code = code;
@@ -45,6 +46,10 @@ public class NetworkTaskError extends Error {
                 return "Connection Failed";
             case CLIENT_ERROR:
                 return "Client Error";
+            case ALREADY_WISHLISTED:
+                return "Item already in wishlist";
+            case ITEM_UNAVAILABLE:
+                return "Item is no longer available";
         }
         return "Unknown Error";
     }
