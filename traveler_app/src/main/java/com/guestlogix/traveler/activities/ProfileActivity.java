0a5174189c52f3dea6fdfbeb10337790fc18600a
Alvin Fong <aktfong@gmail.com>
2019-10-21 14:52:56 -0400

    feat(app): update profileActivity to include wishlist list.

--- traveler_app/src/main/java/com/guestlogix/traveler/activities/ProfileActivity.java
+++ traveler_app/src/main/java/com/guestlogix/traveler/activities/ProfileActivity.java
@@ -4,29 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toolbar;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import com.guestlogix.traveler.R;
 import com.guestlogix.traveler.models.Profile;
-import com.guestlogix.traveler.network.Guest;
 import com.guestlogix.travelercorekit.models.OrderQuery;
 import com.guestlogix.traveleruikit.activities.OrdersActivity;
+import com.guestlogix.traveleruikit.activities.WishlistActivity;
 
 import java.util.Date;
 
 public class ProfileActivity extends AppCompatActivity {
     static final String ARG_PROFILE = "ARG_PROFILE";
 
-    private Profile profile;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        profile = (Profile) getIntent().getSerializableExtra(ARG_PROFILE);
+        Profile profile = (Profile) getIntent().getSerializableExtra(ARG_PROFILE);
 
         if (null == profile) {
             Log.e(this.getLocalClassName(), "No Profile in extras");
@@ -36,19 +31,25 @@ public class ProfileActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_profile);
 
-        View ordersLayout = findViewById(R.id.layout_profileOrders);
-        ordersLayout.setOnClickListener(new View.OnClickListener() {
+        setTitle(profile.getFirstName() + " " + profile.getLastName());
+
+        findViewById(R.id.layout_profileOrders).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), OrdersActivity.class);
-                intent.putExtra(OrdersActivity.ARG_ORDER_QUERY, new OrderQuery(0,10,null, new Date()));
+                intent.putExtra(OrdersActivity.ARG_ORDER_QUERY, new OrderQuery(0, 10, null, new Date()));
                 startActivity(intent);
             }
         });
 
+        findViewById(R.id.layout_profileSavedList).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), WishlistActivity.class));
+            }
+        });
 
-        Button button = findViewById(R.id.button_profile_logout);
-        button.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.button_profile_logout).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 setResult(RESULT_OK);
@@ -56,9 +57,6 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
-        setTitle(profile.getFirstName() + " " + profile.getLastName());
-
-        TextView emailTextView = findViewById(R.id.textView_profileItem_value);
-        emailTextView.setText(profile.getEmail());
+        ((TextView) findViewById(R.id.textView_profileItem_value)).setText(profile.getEmail());
     }
 }
