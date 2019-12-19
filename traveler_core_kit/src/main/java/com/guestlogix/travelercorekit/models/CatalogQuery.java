1d60955f706f2c4f8df74def271e8af1130a3822
Ata N <anamvari@guestlogix.net>
2019-12-19 18:13:23 -0500

    feat(core): Extends CatalogQuery - feat(ui): Adds similar items

--- traveler_core_kit/src/main/java/com/guestlogix/travelercorekit/models/CatalogQuery.java
+++ traveler_core_kit/src/main/java/com/guestlogix/travelercorekit/models/CatalogQuery.java
@@ -7,12 +7,18 @@ import java.util.List;
 public class CatalogQuery implements Serializable {
 
     private List<Flight> flights;
+    private List<Product> products;
 
     public CatalogQuery(List<Flight> flights) {
-        this.flights = flights;
+        this(flights, new ArrayList<>());
     }
 
-    public CatalogQuery() { this(new ArrayList<>()); }
+    public CatalogQuery(List<Flight> flights, List<Product> products) {
+        this.flights = flights;
+        this.products = products;
+    }
+
+    public CatalogQuery() { this(new ArrayList<>(), new ArrayList<>()); }
 
     public List<Flight> getFlights() {
         return flights;
@@ -21,4 +27,8 @@ public class CatalogQuery implements Serializable {
     public void setFlights(List<Flight> flights) {
         this.flights = flights;
     }
+
+    public List<Product> getProducts() { return products; }
+
+    public void setProducts(List<Product> products) { this.products = products; }
 }
